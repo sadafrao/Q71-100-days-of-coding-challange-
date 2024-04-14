7a1b638dfbdf20773e733d0f23097e29f9b2a5c8
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-14 21:30:48 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var age = 29;
+age = 30;
+console.log(age);
+// const variable
+var name1 = "sadaf";
+try {
+    name1 = "rani";
+}
+catch (error) {
+    console.log(error);
+}
---
+++ index.ts
@@ -0,0 +1,10 @@
+let age = 29
+age = 30
+console.log(age);
+// const variable
+const name1 = "sadaf"; 
+try{
+    name1 = "rani";
+}catch(error){
+    console.log(error);
+}
